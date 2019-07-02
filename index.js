cf73acbc74f7d2dd1e39e7199743ba954825a413
Kevin McHale <mchaletkevin@gmail.com>
2019-07-02 19:36:17 -0400

    added a few backend packages as well restructure folders

--- index.js
+++ index.js
@@ -13,6 +13,27 @@ app.get("/api/getList", (req, res) => {
   console.log("Sent list of items");
 });
 
+app.get('/search-location-weather', (req, res) => {
+    const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+    const apiKey = 'a0970f233fe22460b699533253c91eab'
+
+    const userZip = (url1, url2, zip) => {
+        let newUrl = url1 + zip + url2;
+        return newUrl
+    };
+
+    const apiUrl = userZip(baseUrl, apiKey, zipcode);
+
+    fetch(apiUrl)
+        .then(res => res.json())
+        .then(data => {
+            res.send({ data });
+        })
+        .catch(err => {
+            res.redirect('/error');
+        });
+})
+
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
